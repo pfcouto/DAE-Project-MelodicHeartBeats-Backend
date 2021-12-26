8abc9201dea8aad8a606ccffc086e573edbe8b34
Pedro Couto <2191265@my.ipleiria.pt>
2021-12-26 18:03:07 +0000

    Added biometricsType to Administrators Details

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/projetodae/entities/BiometricsType.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/projetodae/entities/BiometricsType.java
@@ -127,6 +127,8 @@ public class BiometricsType implements Serializable {
         this.administrator = administrator;
     }
 
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
